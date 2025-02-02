0612d6da493e74ca4f6864979e95b4896eb7c493
Ohad Redlich <oredlich@paloaltonetworks.com>
2025-02-02 09:55:23 +0200

    use lombok !

--- oauth2-user-app/src/main/java/com/ohadr/oauth2/auth_server_admin_app/web/ClientRegistrationRequest.java
+++ oauth2-user-app/src/main/java/com/ohadr/oauth2/auth_server_admin_app/web/ClientRegistrationRequest.java
@@ -1,5 +1,13 @@
 package com.ohadr.oauth2.auth_server_admin_app.web;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Getter
+@Setter
+@NoArgsConstructor // <--- THIS is it
 public class ClientRegistrationRequest {
     private String clientId;
     private String clientSecret;
@@ -7,44 +15,9 @@ public class ClientRegistrationRequest {
     private String redirectUri;
 
     // Constructors
-    public ClientRegistrationRequest() {}
-
     public ClientRegistrationRequest(String clientId, String clientSecret, String redirectUri) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.redirectUri = redirectUri;
-    }
-
-    // Getters and Setters
-    public String getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
-
-    public void setClientSecret(String clientSecret) {
-        this.clientSecret = clientSecret;
-    }
-
-    public String getRedirectUri() {
-        return redirectUri;
-    }
-
-    public void setRedirectUri(String redirectUri) {
-        this.redirectUri = redirectUri;
-    }
-
-    public String getClientName() {
-        return clientName;
-    }
-
-    public void setClientName(String clientName) {
-        this.clientName = clientName;
     }
 }
