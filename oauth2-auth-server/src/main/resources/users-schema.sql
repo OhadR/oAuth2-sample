ec01c0493b7d778b297e5d9d1d4e306a89679f69
Ohad Redlich <oredlich@paloaltonetworks.com>
2025-02-02 22:33:51 +0200

    add users-schema sql

---
+++ oauth2-auth-server/src/main/resources/users-schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+                       username VARCHAR(50) NOT NULL PRIMARY KEY,
+                       password VARCHAR(100) NOT NULL,
+                       enabled BOOLEAN NOT NULL
+);
+
+CREATE TABLE authorities (
+                             username VARCHAR(50) NOT NULL,
+                             authority VARCHAR(50) NOT NULL,
+                             CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
