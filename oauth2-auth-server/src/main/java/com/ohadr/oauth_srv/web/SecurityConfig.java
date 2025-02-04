4285156b21773764a0b1f95820f21cdd2f2b1251
Ohad Redlich <oredlich@paloaltonetworks.com>
2025-02-04 08:16:17 +0200

    stage 1: let auth-server use JDBC to authenticate users (and PasswordEncoder)

--- oauth2-auth-server/src/main/java/com/ohadr/oauth_srv/web/SecurityConfig.java
+++ oauth2-auth-server/src/main/java/com/ohadr/oauth_srv/web/SecurityConfig.java
@@ -9,12 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -46,7 +51,7 @@ public class SecurityConfig {
                 .formLogin(withDefaults());
         return http.build();
     }
-
+/*
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails userDetails = User.withDefaultPasswordEncoder()
@@ -57,4 +62,18 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(userDetails);
     }
+*/
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        return userDetailsManager;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Ensures passwords are encoded and verified correctly
+    }
+
 }
