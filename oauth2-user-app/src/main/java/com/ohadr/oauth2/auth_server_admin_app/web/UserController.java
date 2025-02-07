c632836d50c4e662d4b89ba5120b831f6c9183d0
Ohad Redlich <oredlich@paloaltonetworks.com>
2025-02-07 23:26:40 +0200

    use log4j 2

--- oauth2-user-app/src/main/java/com/ohadr/oauth2/auth_server_admin_app/web/UserController.java
+++ oauth2-user-app/src/main/java/com/ohadr/oauth2/auth_server_admin_app/web/UserController.java
@@ -1,12 +1,12 @@
 package com.ohadr.oauth2.auth_server_admin_app.web;
 
 import com.ohadr.oauth2.auth_server_admin_app.services.UserService;
-import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Log4j
+@Log4j2
 @RestController
 @RequestMapping("/users")
 public class UserController {
